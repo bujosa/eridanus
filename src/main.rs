0f9219f43d38811567bdd5977ab2b0fe687607f5
bujosa <davidbujosa@gmail.com>
2023-05-03 02:01:18 -0400

    feat: add example using dynamic path

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web::Path, App, HttpResponse, HttpServer, Responder};
+
+use rhai::Engine;
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-// #[get("/hello/{name}")]
-// async fn hello(web::Path(name): web::Path<String>) -> impl Responder {
-//     HttpResponse::Ok().body(format!("Hello {}!", name))
-// }
+/// Example of a dynamic path.
+#[get("/hello/{name}")]
+async fn hello(name: Path<String>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello {}!", name))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(index).service(hello))
         .bind("localhost:8080")?
         .run()
         .await
