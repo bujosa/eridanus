f22108e1bdc19b08236c8e992f2901d3c9f6a7d4
bujosa <davidbujosa@gmail.com>
2023-05-03 01:55:36 -0400

    feat: first example using actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+// #[get("/hello/{name}")]
+// async fn hello(web::Path(name): web::Path<String>) -> impl Responder {
+//     HttpResponse::Ok().body(format!("Hello {}!", name))
+// }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("localhost:8080")?
+        .run()
+        .await
 }
