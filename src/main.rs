c62358fae475d8cbdc9d70a69b7f7ae065c739a2
bujosa <davidbujosa@gmail.com>
2023-05-03 02:27:20 -0400

    feat: add my first example using rhai script

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,20 @@ async fn hello(name: Path<String>) -> impl Responder {
     HttpResponse::Ok().body(format!("Hello {}!", name))
 }
 
+/// Example using Rhai
+#[get("/rhai/{num1}/{num2}")]
+async fn add(path: Path<(i64, i64)>) -> impl Responder {
+    let (num1, num2) = path.into_inner();
+    let mut engine = Engine::new();
+
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result: i64 = engine.eval_file::<i64>("src/add.rhai".into()).unwrap();
+
+    format!("{result}")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(index).service(hello))
